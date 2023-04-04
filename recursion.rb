b238267ffb3abb223560675c0483702899a38fd0
Vitor Cardoso <vitorcnf64@gmail.com>
2023-04-03 23:11:23 -0300

    added recursion.rb, added #fibs

---
+++ recursion.rb
@@ -0,0 +1,37 @@
+# Normal methods to be converted to recursion methods
+class Normal
+  # Fibonacci sequence.
+  def fibs(seq_size)
+    i = 0
+    fibs = []
+
+    while seq_size != i
+      if i.zero?
+        fibs << 0
+        i += 1
+        next
+      elsif i == 1
+        fibs << 1
+        i += 1
+        next
+      end
+
+      fibs.push(fibs[-1] + fibs[-2])
+      i += 1
+    end
+    p fibs
+  end
+end
+
+# Recursion methods in ruby.
+class Recursion
+  def fibs_rec
+    # fibs_rec
+  end
+end
+
+normal = Normal.new
+# recursion = Recursion.new
+
+puts 'Fibonacci Sequence (non-recursive, 16 terms):'
+normal.fibs(16)
