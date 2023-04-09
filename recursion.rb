87b3a7760f7887230d02a5bdc6afa955c9018d01
Vitor Cardoso <vitorcnf64@gmail.com>
2023-04-09 02:39:33 -0300

    added merging for the 2 smallest slices of the to-be-sorted array, ignore the nested if mess, i'm trying to wrap my head around the recursive stuff.

--- recursion.rb
+++ recursion.rb
@@ -32,17 +32,33 @@ class Recursion
     fibs.push(fibs[-1] + fibs[-2])
   end
 
+  # on array arr
+  #   if n < 2
+  # else
+  #   sort left half
+  #   sort right half
+  #   merge
   def merge_sort(arr)
-    # sorted_arr = []
-    if arr.size < 2 # || arr.size == sorted_arr.size
+    if arr.size < 2
       arr
     else
+      arr_l = []
+      arr_r = []
       half = (arr.size / 2).ceil
-      arr1 = []
-      arr2 = []
+      arr.each_slice(half) { |slice| arr_l.empty? ? arr_l = slice : arr_r = slice }
 
-      arr.each_slice(half) { |slice| arr1.empty? ? arr1 = slice : arr2 = slice }
-      # sorted_arr = merge_sort
+      sorted = []
+      arr_l_rec = merge_sort(arr_l)
+      arr_r_rec = merge_sort(arr_r)
+
+      if arr_l_rec.size == 1 && arr_r_rec.size == 1
+        if arr_l_rec[0] < arr_r_rec[0]
+          sorted.push(arr_l_rec, arr_r_rec)
+        else
+          sorted.push(arr_r_rec, arr_l_rec)
+        end
+      end
+      sorted
     end
   end
 end
@@ -54,3 +70,6 @@ puts 'Fibonacci Sequence (non-recursive, 16 terms):'
 normal.fibs(16)
 puts 'Fibonacci Sequence (recursive, 16 terms):'
 p recursion.fibs_rec(16)
+puts 'Merge Sort [353, 526, 907, 31, 783, 933, 120, 680]:'
+p recursion.merge_sort([353, 526, 907, 31, 783, 933, 120, 680])
+# should be 31, 120, 353, 526, 680, 783, 907, 933
